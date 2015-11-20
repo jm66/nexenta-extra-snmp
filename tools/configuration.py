f99856fd25f04b4406c8992ac52c98a9ed60b6d9
Jose Manuel Lopez Lujan <jm.lopez@utoronto.ca>
2015-11-20 17:07:30 -0500

    Working on iostat and iostat-snmp

--- tools/configuration.py
+++ tools/configuration.py
@@ -6,6 +6,7 @@ ZPOOLS_DETAILED_COMMAND = "zpool status %s"
 ZPOOLS_IO_COMMAND = 'zpool iostat %s 1 2 |tail -1'
 ZPOOLS_IO_COMMAND_EXT = "zpool iostat -v %s 1 2| sed '1,3d;$d'"
 
+# IOStat
 IOSTAT_CACHE_FILE = "/tmp/iostat.cache"
 IOSTAT_BASE_OID = '.1.3.6.1.4.1.25359.12'
 IOSTAT_COMMAND = "iostat -xn 1 2 | awk \'n > 1 { print ; next } $NF == \"device\" { n++ }\'"
