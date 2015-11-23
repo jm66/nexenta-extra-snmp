50871bb89c9e7d3e1cd7a8628efa2945f81c5ebc
Jose Manuel Lopez Lujan <jm.lopez@utoronto.ca>
2015-11-23 12:39:11 -0500

    Creating objects with right data types

--- iostat.py
+++ iostat.py
@@ -9,7 +9,6 @@ from tools import IODevice
 
 def iostat():
     # r/s    w/s   kr/s   kw/s wait actv wsvc_t asvc_t  %w  %b device
-    # r/s    w/s   kr/s   kw/s wait actv wsvc_t asvc_t  %w  %b device
     devices = list()
     output = subprocess.Popen(IOSTAT_COMMAND,
                               shell=True,
@@ -19,18 +18,19 @@ def iostat():
         # ignoring first and last element
         device_serial = re.split('\s+', line)[1:-1]
         temp_device = IODevice()
-        temp_device.device = device_serial[10]
-        temp_device.reads_ps = device_serial[0]
-        temp_device.writes_ps = device_serial[1]
-        temp_device.KB_read_ps = device_serial[2]
-        temp_device.KB_written_ps = device_serial[3]
-        temp_device.wait = device_serial[4]
-        temp_device.actv = device_serial[5]
-        temp_device.wsvc_t = device_serial[6]
-        temp_device.asvc_t = device_serial[7]
-        temp_device.wait_pct = device_serial[8]
-        temp_device.busy_pct = device_serial[9]
+        temp_device.device = float(device_serial[10])
+        temp_device.reads_ps = float(device_serial[0])
+        temp_device.writes_ps = float(device_serial[1])
+        temp_device.KB_read_ps = float(device_serial[2])
+        temp_device.KB_written_ps = float(device_serial[3])
+        temp_device.wait = float(device_serial[4])
+        temp_device.actv = float(device_serial[5])
+        temp_device.wsvc_t = float(device_serial[6])
+        temp_device.asvc_t = float(device_serial[7])
+        temp_device.wait_pct = float(device_serial[8])
+        temp_device.busy_pct = float(device_serial[9])
 
+        # appending recently created obj
         devices.append(temp_device)
 
     return [dev.to_json() for dev in devices]
@@ -38,7 +38,7 @@ def iostat():
 
 def main():
     iostats = iostat()
-
+    # saving file
     with open(IOSTAT_CACHE_FILE, 'w') as cache_file:
         json.dump(iostats, cache_file)
 
